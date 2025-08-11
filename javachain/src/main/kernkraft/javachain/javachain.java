388ee058a9c262fd0e722bbb9e303e51f7f290e5
Kernkraftwerk <kernkraftdev@hotmail.com>
2025-08-11 17:01:43 +0200

    upload

---
+++ javachain/src/main/kernkraft/javachain/javachain.java
@@ -0,0 +1,49 @@
+package main.kernkraft.javachain;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+import java.util.List;
+
+public class javachain {
+
+    public  static List<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
+
+
+    public static void main(String[] args){
+        blockchain.add(new Block("First Block","0"));
+        System.out.println("Mining genesis block!");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Second Block", blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Mining block 2");
+        blockchain.get(1).mineBlock(difficulty + 1);
+
+        blockchain.add(new Block("Third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Mining block 3");
+        blockchain.get(2).mineBlock(difficulty + 2);
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
+    // check the validity
+    public static Boolean Validity(List<Block> blockchain) {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Hash not equal!");
+                return false;
+            }
+
+            if (!currentBlock.previousHash.equals(previousBlock.hash)) {
+                System.out.println("Previous hash is not equal!");
+                return false;
+            }
+        }
+        return true;
+    }
+}
